986dcbd8c605a0d493cb0308b37673e3a0d60a86
kongduk <gongduk4960@gmail.com>
2025-04-16 11:43:16 +0900

    i use axios

---
+++ axiostest/src/App copy.jsx
@@ -0,0 +1,11 @@
+import { useEffect,useState } from 'react'
+import axios from "axios"
+import AxiosTest from '../../Day06/src/AxiosTest'
+
+export default function App() {
+  return (
+    <>
+    <AxiosTest />
+    </>
+  )
+}
---
+++ axiostest/src/App.jsx
@@ -0,0 +1,11 @@
+import { useEffect,useState } from 'react'
+import axios from "axios"
+import AxiosTest from './axiosTest'
+
+export default function App() {
+  return (
+    <>
+    <AxiosTest />
+    </>
+  )
+}
