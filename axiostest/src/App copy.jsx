2ee5ebad809ef1bd24077654b21fee8918b125ca
kongduk <gongduk4960@naver.com>
2025-05-14 01:02:06 +0900

    수정

--- axiostest/src/App copy.jsx
+++ axiostest/src/App copy.jsx
@@ -1,11 +1,36 @@
 import { useEffect,useState } from 'react'
 import axios from "axios"
-import AxiosTest from '../../Day06/src/AxiosTest'
 
 export default function App() {
+  const [tests, setTests] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/test")
+      .then((res) => {
+        console.log(res);
+        setTests(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
-    <AxiosTest />
+
+      {
+        tests.map((item) => {
+          return (
+            <>
+              < div >이름 : {item.name} </ div >
+              < div >단계 : {item.desc} </ div >
+              < hr />
+            </>)
+        }
+
+        )
+      }
+
     </>
   )
 }
